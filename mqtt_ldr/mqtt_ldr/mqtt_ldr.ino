a33def3d90b99c0e76d21d02b042523772b62957
Fernando Mazola <fernando_mazola@hotmail.com>
2017-09-21 11:55:48 -0300

    tentando se conectar

--- mqtt_ldr/mqtt_ldr/mqtt_ldr.ino
+++ mqtt_ldr/mqtt_ldr/mqtt_ldr.ino
@@ -3,18 +3,20 @@
 #include <PubSubClient.h>
 
 const int LED = 13;
-//const int LDR = A0;
+const int LDR = A0;
 int entrada = 0;
-const int SENSOR A0;
 
-byte mac [] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0x24}
+
+
+
+byte mac [] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0x24};
 IPAddress server(192, 162, 0, 105);
 
 EthernetClient ethClient;
-PubSubClient client;
+PubSubClient client(ethClient);
 
 void setup() {
-  pinMode(SENSOR, INPUT);
+  pinMode(LDR, INPUT);
   Serial.begin(9600);
   client.setServer(server, 1883);
   Ethernet.begin(mac);
@@ -38,27 +40,18 @@ void reconnect(){
 
 void loop() {
 
-  entrada = analogRead(LDR);
-  
-  delay(500);
-
-  if (entrada < 160) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
-  }
-  delay(1000);
-  Serial.println(entrada);
-
-  
-  if(!client.connected){
+  if(!client.connected()){
    reconnect();
   }
    
   client.loop();
-  
-  float valorDoSensor = analogRead(SENSOR);
+
+  entrada = analogRead(LDR);
+
+  delay(1000);
+  Serial.println(entrada);
+
   char txtValorDoSensor[4];
-  dtostrf(valorDoSensor, 4, 0, txtValorDoSensor);
+  dtostrf(entrada, 4, 0, txtValorDoSensor);
   client.publish("mazola", txtValorDoSensor);
 }
